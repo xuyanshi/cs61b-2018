c3f5ec8676e112316eb8e4d13c907b0b5334b6e4
Yanshi XU <xuyanshi1999@gmail.com>
2022-12-30 12:28:41 +0800

    hw0 Triangle

---
+++ hw0/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+    public static void main(String[] args) {
+        String a = "";
+        for (int i = 0; i < 10; i++) {
+            a += "*";
+            System.out.println(a);
+        }
+    }
+}
