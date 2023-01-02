650f9856dc356d8071daff5660f73ff1787ceb6e
Yanshi XU <xuyanshi1999@gmail.com>
2023-01-02 13:54:21 +0800

    Update NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -11,15 +11,20 @@ public class NBody {
         int numberOfPlanets = in.readInt();
         double radius = in.readDouble();
         Planet[] ps = new Planet[numberOfPlanets];
-        for (int i=0;i<numberOfPlanets;i++) {
+        for (int i = 0; i < numberOfPlanets; i++) {
             double xxPos = in.readDouble();
             double yyPos = in.readDouble();
             double xxVel = in.readDouble();
             double yyVel = in.readDouble();
             double mass = in.readDouble();
             String imgFileName = in.readString();
-            ps[i] = new Planet(xxPos,yyPos,xxVel,yyVel,mass,imgFileName);
+            ps[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
         }
         return ps;
+    }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
     }
 }
