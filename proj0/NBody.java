c866bcba07e6fffdd25aefee4a4252ed1af15519
Yanshi XU <xuyanshi1999@gmail.com>
2023-01-02 20:24:51 +0800

    Creating an Animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -28,24 +28,44 @@ public class NBody {
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         double radius = readRadius(filename);
-        Planet[] ps = readPlanets(filename);
+        Planet[] planets = readPlanets(filename);
 
         String image = "./images/starfield.jpg";
         StdDraw.setScale(-radius, radius);
         StdDraw.clear();
         StdDraw.picture(0, 0, image);
-
-        for(Planet p:ps) {
+        for (Planet p : planets) {
             p.draw();
         }
         StdDraw.show();
         //java NBody 157788000.0 25000.0 data/planets.txt
 
         StdDraw.enableDoubleBuffering();
-        double t =0.0;
-        while(t<T) {
-            StdDraw.pause((int)dt);
-            t+=dt;
+        double t = 0.0;
+        while (t < T) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int i = 0; i < planets.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, image);
+            for (Planet p : planets) {
+                p.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            t += dt;
+        }
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
         }
     }
 }
