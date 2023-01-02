d53b2725ce14b50a6b10907b9ec9c43bde22feb3
Yanshi XU <xuyanshi1999@gmail.com>
2023-01-02 13:52:46 +0800

    ReadPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,8 +10,16 @@ public class NBody {
         In in = new In(path);
         int numberOfPlanets = in.readInt();
         double radius = in.readDouble();
+        Planet[] ps = new Planet[numberOfPlanets];
         for (int i=0;i<numberOfPlanets;i++) {
-            
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            ps[i] = new Planet(xxPos,yyPos,xxVel,yyVel,mass,imgFileName);
         }
+        return ps;
     }
 }
