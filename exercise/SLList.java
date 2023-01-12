51c2c0afce3845c186597bd7880cbb3a71c6fd17
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-12 22:10:54 +0800

    exercise 2.2.3

--- exercise/SLList.java
+++ exercise/SLList.java
@@ -1,16 +1,51 @@
 public class SLList {
+    public static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
     private IntNode first;
+    private int size;
+
+    public SLList() {
+        first = null;
+        size = 0;
+    }
 
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
     }
 
     public void addFirst(int x) {
+        size++;
         first = new IntNode(x, first);
     }
 
     public int getFirst() {
         return first.item;
+    }
+
+    public void addLast(int x) {
+        this.size++;
+        if (size == 1) {
+            first = new IntNode(x, first);
+        } else {
+            IntNode ptr = first;
+            while (ptr.next != null) {
+                ptr = ptr.next;
+            }
+            ptr.next = new IntNode(x, null);
+        }
+    }
+
+    public int size() {
+        return this.size;
     }
 
     public void print() {
@@ -24,9 +59,8 @@ public class SLList {
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList(15);
-        L.addFirst(10);
-        L.addFirst(5);
+        SLList L = new SLList();
+        L.addLast(10);
         L.print();
     }
 }
