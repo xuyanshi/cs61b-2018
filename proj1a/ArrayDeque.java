627ce5112daf46e4310a513d22503fec3c7bfc23
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-12 14:43:20 +0800

    Project 1A : Data Structures Init

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,41 @@
+public class ArrayDeque<T> {
+    private int size;
+
+    public ArrayDeque() {
+        this.size = 8;
+    }
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+
+        return null;
+    }
+
+    public T get(int index) {
+
+        return null;
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,43 @@
+public class LinkedListDeque<T> {
+    private int size;
+
+    public LinkedListDeque() {
+        this.size = 0;
+    }
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T getRecursive(int index) {
+        return null;
+    }
+}
