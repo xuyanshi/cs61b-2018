b5a8055f842fb8de54e682c563f53b57544d3522
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-13 12:08:05 +0800

    Naive AList

---
+++ exercise/AList.java
@@ -0,0 +1,32 @@
+public class AList {
+    int[] items;
+    int size;
+
+    public AList() {
+        items = new int[100];
+        size = 0;
+    }
+
+    public void addLast(int x) {
+        items[size] = x;
+        size++;
+    }
+
+    public int getLast() {
+        return items[size - 1];
+    }
+
+    public int get(int i) {
+        return items[i];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int removeLast() {
+        int x = getLast();
+        size--;
+        return x;
+    }
+}
