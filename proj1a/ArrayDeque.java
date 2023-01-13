311fa17409d7997f4bed6662a93a75a315af63ee
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-14 02:03:39 +0800

    Naive ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -7,15 +7,27 @@ public class ArrayDeque<T> {
     public ArrayDeque() {
         size = 0;
         start = 0;
-        capacity = 8;
+        // capacity = 8;
+        capacity = 10000;
         items = (T[]) new Object[capacity];
     }
 
+    private int prevOne(int i) {
+        return (i - 1 + capacity) % capacity;
+    }
+
+    private int nextOne(int i) {
+        return (i + 1 + capacity) % capacity;
+    }
+
     public void addFirst(T item) {
+        start = prevOne(start);
+        items[start] = item;
         size++;
     }
 
     public void addLast(T item) {
+        items[(start + size) % capacity] = item;
         size++;
     }
 
@@ -24,11 +36,15 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        for (int i = start; i < start + size; i++) {
+        for (int i = start; i < start + size && i < capacity; i++) {
             System.out.print(items[i]);
             System.out.print(" ");
-            System.out.println();
         }
+        for (int i = 0; i < start + size - capacity && i < capacity; i++) {
+            System.out.print(items[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 
     public int size() {
@@ -36,17 +52,25 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T removeItem = items[start];
+        start = nextOne(start);
         size--;
-        return null;
+        return removeItem;
     }
 
     public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T removeItem = items[(start + size - 1) % capacity];
         size--;
-        return null;
+        return removeItem;
     }
 
     public T get(int index) {
-
-        return null;
+        return items[(index + start) % capacity];
     }
 }
