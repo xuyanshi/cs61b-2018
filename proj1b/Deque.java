b628a8d7f4e6be6c77f1605512bbdfed376983cb
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-14 23:19:48 +0800

    Update Deque.java

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,10 +1,16 @@
 public interface Deque<T> {
     void addFirst(T item);
+
     void addLast(T item);
+
     boolean isEmpty();
+
     int size();
+
     void printDeque();
+
     T removeFirst();
+
     T removeLast();
 
     T get(int index);
