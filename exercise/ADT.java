eb09953a12803c5df6f1fd551a66efccd7e92a35
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-16 21:11:40 +0800

    Update ADT.java

--- exercise/ADT.java
+++ exercise/ADT.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ADT {
+    /* Exercise 4.4.1 */
+    public List<String> getWords(String inputFilename) {
+        List<String> lst = new ArrayList<String>();
+        // In in = new In();
+        return lst;
+    }
+
+    /* Exercise 4.4.2 */
+    public int countUniqueWords(List<String> lst) {
+        return 0;
+    }
 
     public static void main(String[] args) {
 
