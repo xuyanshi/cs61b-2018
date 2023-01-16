b3b7b0a96f5adf1702b649dbaaa8774189cb3634
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-17 02:21:51 +0800

    Proj1Gold init

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void addFirst() {
+    }
+
+    @Test
+    public void addLast() {
+    }
+
+    @Test
+    public void removeFirst() {
+    }
+
+    @Test
+    public void removeLast() {
+    }
+
+    @Test
+    public void printDeque() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void get() {
+    }
+}
