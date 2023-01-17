fcbb00209cdd9160043690e7b6bd937508a630ba
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-17 10:49:16 +0800

    proj1gold task1

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,38 +1,33 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class TestArrayDequeGold {
 
     @Test
-    public void addFirst() {
+    public void TestRandom() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+        for (int i = 0; i < 1000; i++) {
+            int randomInt = StdRandom.uniform(100);
+            double randomMethod = StdRandom.uniform(-2.0, 2.0);
+            if (randomMethod < -1 && sad1.size() > 0) {
+                Integer s1 = sad1.removeFirst();
+                Integer a1 = ads1.removeFirst();
+                assertEquals(s1, a1);
+            } else if (randomMethod < 0 && sad1.size() > 0) {
+                Integer s1 = sad1.removeLast();
+                Integer a1 = ads1.removeLast();
+                assertEquals(s1, a1);
+            } else if (randomMethod < 1) {
+                sad1.addLast(randomInt);
+                ads1.addLast(randomInt);
+            } else if (randomMethod < 2) {
+                sad1.addFirst(randomInt);
+                ads1.addFirst(randomInt);
+            }
+        }
     }
 
-    @Test
-    public void addLast() {
-    }
-
-    @Test
-    public void removeFirst() {
-    }
-
-    @Test
-    public void removeLast() {
-    }
-
-    @Test
-    public void printDeque() {
-    }
-
-    @Test
-    public void isEmpty() {
-    }
-
-    @Test
-    public void size() {
-    }
-
-    @Test
-    public void get() {
-    }
 }
