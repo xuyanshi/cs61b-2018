29cdc32f1e7e2da2e8a6b0a3092fbfb3ea09b68e
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-17 19:24:19 +0800

    lab5 task1

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,5 +1,7 @@
 package byog.lab5;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import byog.TileEngine.TERenderer;
@@ -12,4 +14,42 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 60;
+
+    public static void addHexagon(int sideLength) {
+
+    }
+
+    public static void fillWithHexagons(TETile[][] tiles) {
+/*
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                tiles[x][y] = randomTile();
+            }
+        }
+*/
+    }
+
+    public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH * HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        // fills in a block with Hexagons
+
+
+        // draws the world to the screen
+        ter.renderFrame(world);
+    }
 }
