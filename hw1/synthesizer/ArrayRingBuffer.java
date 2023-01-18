ba87aaba698133082e9caf814b6b02a462787ae2
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-18 17:52:51 +0800

    hw1 task3

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -67,7 +67,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
-        return rb[first];
+        try {
+            assert !isEmpty();
+            return rb[first];
+        } catch (Exception e) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
