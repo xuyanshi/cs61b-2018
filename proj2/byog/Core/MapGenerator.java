dce7f9dc59260d2a81091f0dd79b3cb39535c9ea
Genshi KOMATSU <xuyanshi1999@gmail.com>
2023-01-25 20:31:57 +0800

    Update MapGenerator.java

--- proj2/byog/Core/MapGenerator.java
+++ proj2/byog/Core/MapGenerator.java
@@ -2,19 +2,29 @@ package byog.Core;
 
 import byog.TileEngine.TETile;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MapGenerator {
-    private final int WIDTH = 0;
-    private final int HEIGHT = 0;
+public class MapGenerator implements Serializable {
+    private int WIDTH = 0;
+    private int HEIGHT = 0;
     private Random RANDOM;
     private long SEED;
     private static final int TIMES = 100;
     private static TETile[][] world;
     private static TETile[][] positionOfRoom;
     private static ArrayList<Room> Rooms;
-    
+
+    public MapGenerator(int width, int height, long seed) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.SEED = seed;
+        this.RANDOM = new Random(seed);
+
+        boolean[][] isPassed = new boolean[width][height];
+        
+    }
 
     public static void main(String[] args) {
         System.out.println("MapGenerator");
