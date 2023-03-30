39bae8d68569a391eb19225dc871b02400b7caf7
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-30 21:35:43 +0800

    PercolationStats init

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,38 @@
 package hw2;
 
 public class PercolationStats {
+    /**
+     * perform T independent experiments on an N-by-N grid
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
 
+    }
+
+    /**
+     * sample mean of percolation threshold
+     */
+    public double mean() {
+        return 0.0;
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return 0.0;
+    }
+
+    /**
+     * low endpoint of 95% confidence interval
+     */
+    public double confidenceLow() {
+        return 0.0;
+    }
+
+    /**
+     * high endpoint of 95% confidence interval
+     */
+    public double confidenceHigh() {
+        return 0.0;
+    }
 }
