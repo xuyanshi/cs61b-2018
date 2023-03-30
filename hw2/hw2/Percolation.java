151915885bb61ff0be449aa1798bbbda8d4d48fb
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-30 20:24:14 +0800

    Update Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,9 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * @author xuyanshi
+ */
 public class Percolation {
     /**
      * create N-by-N grid, with all sites initially blocked
