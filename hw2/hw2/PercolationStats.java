f1e6a378bad269ea9424ba4065a217965fa10ee6
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-31 00:03:54 +0800

    Update PercolationStats.java

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -2,17 +2,25 @@ package hw2;
 
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.
 
 public class PercolationStats {
-    private double mean, stddev, confidenceLow, confidenceHigh;
+    private double x[];
+    private int N, T;
 
     /**
      * perform T independent experiments on an N-by-N grid
      */
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N <= 0 or T <= 0");
+        }
+        this.N = N;
+        this.T = T;
+        this.x = new double[N];
         for (int i = 0; i < T; i++) {
             Percolation per = pf.make(N);
-
+            
         }
     }
 
@@ -20,27 +28,27 @@ public class PercolationStats {
      * sample mean of percolation threshold
      */
     public double mean() {
-        return mean;
+        return StdStats.mean(x);
     }
 
     /**
      * sample standard deviation of percolation threshold
      */
     public double stddev() {
-        return stddev;
+        return StdStats.stddev(x);
     }
 
     /**
      * low endpoint of 95% confidence interval
      */
     public double confidenceLow() {
-        return confidenceLow;
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
     }
 
     /**
      * high endpoint of 95% confidence interval
      */
     public double confidenceHigh() {
-        return confidenceHigh;
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
     }
 }
