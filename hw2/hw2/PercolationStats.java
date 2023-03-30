77b19847dbdae0815adf2475fb04984c36fe7cbe
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-30 21:38:04 +0800

    update

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -7,7 +7,9 @@ public class PercolationStats {
      * perform T independent experiments on an N-by-N grid
      */
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        for (int i = 0; i < T; i++) {
 
+        }
     }
 
     /**
