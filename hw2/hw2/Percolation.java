155454ff476bfee8e8cf1beaaa7a4717a062a228
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-30 20:10:36 +0800

    update hw2 config

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,54 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /**
+     * create N-by-N grid, with all sites initially blocked
+     *
+     * @param N N-by-N grid
+     */
+    public Percolation(int N) {
 
+    }
+
+    /**
+     * open the site (row, col) if it is not open already
+     */
+    public void open(int row, int col) {
+
+    }
+
+    /**
+     * is the site (row, col) open?
+     */
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    /**
+     * is the site (row, col) full?
+     */
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    /**
+     * does the system percolate?
+     *
+     * @return percolates
+     */
+    public boolean percolates() {
+        return false;
+    }
+
+    /**
+     * use for unit testing (not required)
+     */
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
 }
