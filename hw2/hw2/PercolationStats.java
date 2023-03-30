fbaf318fb78c2a7420d986593c32484e91b5bc28
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-30 21:42:30 +0800

    Update PercolationStats.java

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -8,7 +8,7 @@ public class PercolationStats {
      */
     public PercolationStats(int N, int T, PercolationFactory pf) {
         for (int i = 0; i < T; i++) {
-
+            Percolation p = pf.make(N);
         }
     }
 
