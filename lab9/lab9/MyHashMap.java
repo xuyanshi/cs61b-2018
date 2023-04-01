8d25f6c0b8b20edfbafa6b730af89899d7e646d0
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-01 16:08:47 +0800

    MyHashMap basic functions

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -59,19 +59,24 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int keyHash = hash(key);
+        return buckets[keyHash % buckets.length].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int keyHash = hash(key);
+        if (!buckets[keyHash % buckets.length].containsKey(key)) {
+            size++;
+        }
+        buckets[keyHash % buckets.length].put(key, value);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
