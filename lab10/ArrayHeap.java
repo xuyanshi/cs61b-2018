641b8c5ca6d2a1dcc078b9e7f9a9ad6373b26f85
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-02 20:25:14 +0800

    Update ArrayHeap.java

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -28,24 +28,21 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Returns the index of the node to the left of the node at i.
      */
     private static int leftIndex(int i) {
-        /* TODO: Your code here! */
-        return 0;
+        return 2 * i;
     }
 
     /**
      * Returns the index of the node to the right of the node at i.
      */
     private static int rightIndex(int i) {
-        /* TODO: Your code here! */
-        return 0;
+        return 2 * i + 1;
     }
 
     /**
      * Returns the index of the node that is the parent of the node at i.
      */
     private static int parentIndex(int i) {
-        /* TODO: Your code here! */
-        return 0;
+        return i / 2;
     }
 
     /**
