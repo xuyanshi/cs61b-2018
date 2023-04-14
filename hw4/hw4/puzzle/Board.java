04a258b6e436b1faea11f2c9668e067483abb590
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-14 20:51:37 +0800

    Update Board.java

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,21 +1,54 @@
 package hw4.puzzle;
 
 public class Board {
+    public Board(int[][] tiles) {
+        
+    }
 
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
-    /*public String toString() {
+    public int tileAt(int i, int j) {
+        return 0;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public Iterable<WorldState> neighbors() {
+        return null;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public int estimatedDistanceToGoal() {
+        return 0;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    /**
+     * Returns the string representation of the board.
+     * Uncomment this method.
+     */
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
+                s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
