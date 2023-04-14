5b2057b4a96f75a9449f34fabe3591205b196472
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-14 13:51:37 +0800

    Update Solver.java

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,4 +1,17 @@
 package hw4.puzzle;
 
 public class Solver {
+    public Solver(WorldState initial) {
+
+
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<WorldState> solution() {
+        return null;
+    }
+
 }
