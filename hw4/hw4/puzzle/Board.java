f171984ac199225a0e409ce1f78d1245619e5051
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-14 20:53:19 +0800

    Update Board.java

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,26 +1,46 @@
 package hw4.puzzle;
 
 public class Board {
+    /**
+     * Board(tiles): Constructs a board from an N-by-N array of tiles where
+     * tiles[i][j] = tile at row i, column j
+     */
     public Board(int[][] tiles) {
-        
+
     }
 
+    /**
+     * tileAt(i, j): Returns value of tile at row i, column j (or 0 if blank)
+     */
     public int tileAt(int i, int j) {
         return 0;
     }
 
+    /**
+     * size(): Returns the board size N
+     */
     public int size() {
         return 0;
     }
 
+    /**
+     * neighbors():  Returns the neighbors of the current board
+     */
     public Iterable<WorldState> neighbors() {
         return null;
     }
 
+    /**
+     * hamming(): Hamming estimate described below
+     */
     public int hamming() {
         return 0;
     }
 
+
+    /**
+     * manhattan():  Manhattan estimate described below
+     */
     public int manhattan() {
         return 0;
     }
