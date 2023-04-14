0744bcb218048bd78ef68423c2bd138c9a11b322
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-14 23:50:46 +0800

    compile

--- hw4/hw4/puzzle/TestBoard.java
+++ hw4/hw4/puzzle/TestBoard.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestBoard {
-    /*
     @Test
     public void verifyImmutability() {
         int r = 2;
@@ -27,6 +26,4 @@ public class TestBoard {
         x[1][1] = 1000;
         assertEquals("Your Board class is mutable and you should be making a copy of the values in the passed tiles array. Please see the FAQ!", 3, b.tileAt(1, 1));
     }
-
-     */
 } 
