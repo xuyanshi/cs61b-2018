c0f13c43ad6951d081dd9ba3cb85ce4d74bb0abb
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-16 21:07:26 +0800

    Update MazeBreadthFirstPaths.java

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,13 +9,19 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int source;
+    private int target;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
     }
