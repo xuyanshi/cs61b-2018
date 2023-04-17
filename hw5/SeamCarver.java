c55b6c71489707756915bfd918a962f779353caa
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-17 19:46:14 +0800

    skeleton

---
+++ hw5/SeamCarver.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    public SeamCarver(Picture picture) {
+    }
+
+    // current picture
+    public Picture picture() {
+        return null;
+    }
+
+    // width of current picture
+    public int width() {
+        return 0;
+    }
+
+    // height of current picture
+    public int height() {
+        return 0;
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0.0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from picture
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    // remove vertical seam from picture
+    public void removeVerticalSeam(int[] seam) {
+    }
+}
