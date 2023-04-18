0e7000f14d9a65cb6fa249aaa184faa18f53e133
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-18 13:11:32 +0800

    Generating a SawTooth init

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -7,15 +7,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         /* Your code here. */
-        Generator g1 = new SineWaveGenerator(200);
-        Generator g2 = new SineWaveGenerator(201);
-
-        ArrayList<Generator> generators = new ArrayList<Generator>();
-        generators.add(g1);
-        generators.add(g2);
-        MultiGenerator mg = new MultiGenerator(generators);
-
-        GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(mg);
-        gav.drawAndPlay(500000, 1000000);
+        Generator generator = new SawToothGenerator(512);
+        GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
+        gav.drawAndPlay(4096, 1000000);
     }
 } 
--- lab14/lab14/SawToothGenerator.java
+++ lab14/lab14/SawToothGenerator.java
@@ -1,8 +1,26 @@
 package lab14;
 
+import lab14lib.Generator;
+
 /**
  * @author xuyanshi
  * @date 2023/4/18 13:05
  */
-public class SawToothGenerator {
+public class SawToothGenerator implements Generator {
+
+    private double frequency;
+    private int state;
+
+    public SawToothGenerator(double frequency) {
+        state = 0;
+        this.frequency = frequency;
+    }
+
+    /**
+     * Returns a number between -1 and 1
+     */
+    @Override
+    public double next() {
+        return 0;
+    }
 }
