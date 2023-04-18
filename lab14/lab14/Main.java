bc7aeb3e15e72fa84edfc02268056685e992fc4d
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-18 12:57:23 +0800

    Update Main.java

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -1,13 +1,14 @@
 package lab14;
 
 import lab14lib.Generator;
+import lab14lib.GeneratorDrawer;
 import lab14lib.GeneratorPlayer;
 
 public class Main {
     public static void main(String[] args) {
         /* Your code here. */
-        Generator generator = new SineWaveGenerator(440);
-        GeneratorPlayer gp = new GeneratorPlayer(generator);
-        gp.play(1000000);
+        Generator generator = new SineWaveGenerator(200);
+        GeneratorDrawer gd = new GeneratorDrawer(generator);
+        gd.draw(4096);
     }
 } 
