5813bccb3bb380b8a6e1bcf89542e3a71429375d
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-18 12:01:55 +0800

    Update Generator.java

--- lab14/lab14lib/Generator.java
+++ lab14/lab14lib/Generator.java
@@ -1,6 +1,8 @@
 package lab14lib;
 
 public interface Generator {
-	/** Returns a number between -1 and 1 */
-	double next();
+    /**
+     * Returns a number between -1 and 1
+     */
+    double next();
 }
