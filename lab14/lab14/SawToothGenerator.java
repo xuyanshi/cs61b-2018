7e40557474dfb965b375dc95fb4497d5ae420db3
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-18 13:37:47 +0800

    Update SawToothGenerator.java

--- lab14/lab14/SawToothGenerator.java
+++ lab14/lab14/SawToothGenerator.java
@@ -9,7 +9,7 @@ import lab14lib.Generator;
  */
 public class SawToothGenerator implements Generator {
 
-    private int period;
+    private final int period;
     private int state;
 
     public SawToothGenerator(int period) {
