19297376a08a8397632de10da35e65ae5b56e8d1
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-20 03:59:33 +0800

    Update BinaryTrie.java

--- hw7/BinaryTrie.java
+++ hw7/BinaryTrie.java
@@ -6,14 +6,29 @@ import java.util.Map;
  * @date 2023/4/20 03:55
  */
 public class BinaryTrie implements Serializable {
+    /**
+     * Constructor.
+     * Given a frequency table which maps symbols of type V to their relative frequencies,
+     * the constructor should build a Huffman decoding trie according to the procedure discussed in class.
+     * You may find implementations of Huffman codes on the web useful for inspiration.
+     */
     public BinaryTrie(Map<Character, Integer> frequencyTable) {
 
     }
 
+    /**
+     * longestPrefixMatch.
+     * The longestPrefixMatch method finds the longest prefix that matches the given querySequence
+     * and returns a Match object for that Match.
+     */
     public Match longestPrefixMatch(BitSequence querySequence) {
         return null;
     }
 
+    /**
+     * buildLookupTable.
+     * The buildLookupTable method returns the inverse of the coding trie.
+     */
     public Map<Character, BitSequence> buildLookupTable() {
         return null;
     }
