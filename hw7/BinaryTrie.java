0a38e7bfb7f68a0804cbf80be013b6203e914a1a
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-20 03:55:43 +0800

    Update BinaryTrie.java

--- hw7/BinaryTrie.java
+++ hw7/BinaryTrie.java
@@ -1,6 +1,20 @@
+import java.io.Serializable;
+import java.util.Map;
+
 /**
  * @author xuyanshi
  * @date 2023/4/20 03:55
  */
-public class BinaryTrie {
+public class BinaryTrie implements Serializable {
+    public BinaryTrie(Map<Character, Integer> frequencyTable) {
+
+    }
+
+    public Match longestPrefixMatch(BitSequence querySequence) {
+        return null;
+    }
+
+    public Map<Character, BitSequence> buildLookupTable() {
+        return null;
+    }
 }
