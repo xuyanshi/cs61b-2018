c4add29d0c6bf5fb7b1ac7b4a680b61bd4806f91
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-20 03:54:51 +0800

    Update TestBinaryTrie.java

--- hw7/TestBinaryTrie.java
+++ hw7/TestBinaryTrie.java
@@ -10,7 +10,8 @@ import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestBinaryTrie {
-/* Uncomment when you are ready. 
+    /* Uncomment when you are ready.
+     */
     @Test
     public void bestPrefixMatchWithExactMatches() {
         System.out.println("Testing that bestPrefixMatch works with exact matches.");
@@ -24,18 +25,18 @@ public class TestBinaryTrie {
 
         BitSequence shouldBeA = new BitSequence("000");
         Match m = trie.longestPrefixMatch(shouldBeA);
-        assertEquals((char) 'a', (char) m.getSymbol()); 
+        assertEquals((char) 'a', (char) m.getSymbol());
         assertEquals(shouldBeA, m.getSequence());
 
         BitSequence shouldBeE = new BitSequence("11");
         m = trie.longestPrefixMatch(shouldBeE);
-        assertEquals((char) 'e', (char) m.getSymbol()); 
+        assertEquals((char) 'e', (char) m.getSymbol());
         assertEquals(shouldBeE, m.getSequence());
     }
 
     @Test
     public void bestPrefixMatchWithLongerBitSequences() {
-        System.out.println("Testing that bestPrefixMatch works with long string matches.");        
+        System.out.println("Testing that bestPrefixMatch works with long string matches.");
         Map<Character, Integer> frequencyTable = new HashMap<Character, Integer>();
         frequencyTable.put('a', 1);
         frequencyTable.put('b', 2);
@@ -46,21 +47,21 @@ public class TestBinaryTrie {
 
         BitSequence shouldBeA = new BitSequence("0001010001");
         Match m = trie.longestPrefixMatch(shouldBeA);
-        assertEquals((char) 'a', (char) m.getSymbol()); 
+        assertEquals((char) 'a', (char) m.getSymbol());
         BitSequence justA = new BitSequence("000");
         assertEquals(justA, m.getSequence());
 
         BitSequence shouldBeE = new BitSequence("11000110001");
         m = trie.longestPrefixMatch(shouldBeE);
-        assertEquals((char) 'e', (char) m.getSymbol()); 
+        assertEquals((char) 'e', (char) m.getSymbol());
         BitSequence justE = new BitSequence("11");
         assertEquals(justE, m.getSequence());
 
         BitSequence shouldBeAE = new BitSequence("00011");
         m = trie.longestPrefixMatch(shouldBeA);
-        assertEquals((char) 'a', (char) m.getSymbol()); 
+        assertEquals((char) 'a', (char) m.getSymbol());
         assertEquals(justA, m.getSequence());
-        
+
         BitSequence remainingBits = shouldBeAE.allButFirstNBits(m.getSequence().length());
         m = trie.longestPrefixMatch(remainingBits);
         assertEquals((char) 'e', (char) m.getSymbol());
@@ -68,14 +69,14 @@ public class TestBinaryTrie {
     }
 
     @Test
-    public void testYourLookupTable() {   
-        System.out.println("Testing that your code outputs the right lookup table."); 
+    public void testYourLookupTable() {
+        System.out.println("Testing that your code outputs the right lookup table.");
         Map<Character, Integer> frequencyTable = new HashMap<Character, Integer>();
         frequencyTable.put('a', 1);
         frequencyTable.put('b', 2);
         frequencyTable.put('c', 4);
         frequencyTable.put('d', 5);
-        frequencyTable.put('e', 6);        
+        frequencyTable.put('e', 6);
         BinaryTrie trie = new BinaryTrie(frequencyTable);
         Map<Character, BitSequence> yourTable = trie.buildLookupTable();
 
@@ -90,6 +91,5 @@ public class TestBinaryTrie {
 
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestBinaryTrie.class);
-    }    
-    */
+    }
 } 
