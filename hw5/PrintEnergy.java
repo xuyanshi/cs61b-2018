ed4cf78d119864f26353828717a257d7e732c24f
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-21 16:53:14 +0800

    test main

--- hw5/PrintEnergy.java
+++ hw5/PrintEnergy.java
@@ -2,11 +2,9 @@
  *  Compilation:  javac PrintEnergy.java
  *  Execution:    java PrintEnergy input.png
  *  Dependencies: SeamCarver.java
- *                
- *
  *  Read image from file specified as command line argument.
- *  Print energy of each pixel as calculated by SeamCarver object. 
- * 
+ *  Print energy of each pixel as calculated by SeamCarver object.
+ *
  ******************************************************************************/
 
 import edu.princeton.cs.algs4.Picture;
@@ -17,10 +15,10 @@ public class PrintEnergy {
     public static void main(String[] args) {
         Picture picture = new Picture(args[0]);
         StdOut.printf("%d-by-%d image\n", picture.width(), picture.height());
-        
+
         SeamCarver sc = new SeamCarver(picture);
-        
-        StdOut.printf("Printing energy calculated for each pixel.\n");        
+
+        StdOut.printf("Printing energy calculated for each pixel.\n");
 
         for (int row = 0; row < sc.height(); row++) {
             for (int col = 0; col < sc.width(); col++)
