debc60f07dafc93786cbe49ec8f498d966c6d265
xuyanshi <xuyanshi@ruc.edu.cn>
2023-04-21 23:57:32 +0800

    energy PASSED

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.Color;
 import java.util.Arrays;
 
 public class SeamCarver {
@@ -34,12 +35,32 @@ public class SeamCarver {
         return height;
     }
 
+
+    private double squareSum(double num1, double num2, double num3) {
+        return num1 * num1 + num2 * num2 + num3 * num3;
+    }
+
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
         if (energies[x][y] >= 0) {
             return energies[x][y];
         }
         double deltaX2 = 0.0, deltaY2 = 0.0, ans;
+
+        int xAddOne = (x + 1 < width) ? (x + 1) : 0;
+        int xSubOne = (x - 1 >= 0) ? x - 1 : width - 1;
+        int yAddOne = (y + 1 < height) ? y + 1 : 0;
+        int ySubOne = (y - 1 >= 0) ? y - 1 : height - 1;
+
+        Color xAddOneColor = picture.get(xAddOne, y);
+        Color xSubOneColor = picture.get(xSubOne, y);
+        Color yAddOneColor = picture.get(x, yAddOne);
+        Color ySubOneColor = picture.get(x, ySubOne);
+
+        deltaX2 = squareSum(xAddOneColor.getRed() - xSubOneColor.getRed(),
+                xAddOneColor.getGreen() - xSubOneColor.getGreen(), xAddOneColor.getBlue() - xSubOneColor.getBlue());
+        deltaY2 = squareSum(yAddOneColor.getRed() - ySubOneColor.getRed(),
+                yAddOneColor.getGreen() - ySubOneColor.getGreen(), yAddOneColor.getBlue() - ySubOneColor.getBlue());
         ans = deltaX2 + deltaY2;
         energies[x][y] = ans;
         return ans;
@@ -64,6 +85,8 @@ public class SeamCarver {
     }
 
     public static void main(String[] args) {
-
+        int rgb = 0;
+        rgb = new java.awt.Color(8, 8, 8).getRed();
+        System.out.println(Integer.toString(rgb, 2));
     }
 }
