61c8c33ce82eaa6c2955f00e1202d1ee3c3faca2
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 20:30:30 +0800

    Update HuffmanDecoder.java

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -14,13 +14,19 @@ public class HuffmanDecoder {
         int number = (Integer) numberObject;
         // 3: Read the massive bit sequence corresponding to the original txt.
         Object hugeBitSequenceObject = or.readObject();
-        BitSequence hugeBitSequence = (BitSequence) hugeBitSequenceObject;
+        BitSequence bs = (BitSequence) hugeBitSequenceObject;
         // 4: Repeat until there are no more symbols:
         //    4a: Perform a longest prefix match on the massive sequence.
         //    4b: Record the symbol in some data structure.
         //    4c: Create a new bit sequence containing the remaining unmatched bits.
         char[] outputs = new char[number];
-        
+        int decodedChars = 0;
+        while (decodedChars < number) {
+            Match match = trie.longestPrefixMatch(bs);
+            outputs[decodedChars] = match.getSymbol();
+            bs = bs.allButFirstNBits(match.getSequence().length());
+            ++decodedChars;
+        }
         // 5: Write the symbols in some data structure to the specified file.
         String outputFilename = args[1];
         FileUtils.writeCharArray(outputFilename, outputs);
