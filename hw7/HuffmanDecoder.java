99fc1e33513dd65e0c23ad0b39642c65ae484077
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 20:17:30 +0800

    object cast

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -7,11 +7,14 @@ public class HuffmanDecoder {
         // 1: Read the Huffman coding trie.
         String filename = args[0];
         ObjectReader or = new ObjectReader(filename);
-        Object trie = or.readObject();
+        Object trieObject = or.readObject();
+        BinaryTrie trie = (BinaryTrie) trieObject;
         // 2: If applicable, read the number of symbols.
-        Object number = or.readObject();
+        Object numberObject = or.readObject();
+        int number = (Integer) numberObject;
         // 3: Read the massive bit sequence corresponding to the original txt.
-        Object hugeBitSequence = or.readObject();
+        Object hugeBitSequenceObject = or.readObject();
+        BitSequence hugeBitSequence = (BitSequence) hugeBitSequenceObject;
         // 4: Repeat until there are no more symbols:
         //    4a: Perform a longest prefix match on the massive sequence.
         //    4b: Record the symbol in some data structure.
