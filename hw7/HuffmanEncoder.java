f7e4b63f3aa6dcb836d108479b0be1b72f1b757e
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 19:43:10 +0800

    update main

--- hw7/HuffmanEncoder.java
+++ hw7/HuffmanEncoder.java
@@ -15,8 +15,14 @@ public class HuffmanEncoder {
     }
 
     public static void main(String[] args) {
+        // 1: Read the file as 8 bit symbols.
         String filename = args[0];
-        char[] inputs = new char[1000];
-        BinaryTrie bt = new BinaryTrie(buildFrequencyTable(inputs));
+        char[] inputs = FileUtils.readFile(filename);
+        // 2: Build frequency table.
+        Map<Character, Integer> frequencyTable = buildFrequencyTable(inputs);
+        // 3: Use frequency table to construct a binary decoding trie.
+        BinaryTrie bt = new BinaryTrie(frequencyTable);
+        // 4: Write the binary decoding trie to the .huf file.
+        
     }
 }
