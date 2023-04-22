88fef4d488d00d01a34d34b55e6bd617f6bb21cd
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 20:23:09 +0800

    Update HuffmanDecoder.java

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -19,8 +19,10 @@ public class HuffmanDecoder {
         //    4a: Perform a longest prefix match on the massive sequence.
         //    4b: Record the symbol in some data structure.
         //    4c: Create a new bit sequence containing the remaining unmatched bits.
-
+        char[] outputs = new char[number];
+        
         // 5: Write the symbols in some data structure to the specified file.
-
+        String outputFilename = args[1];
+        FileUtils.writeCharArray(outputFilename, outputs);
     }
 }
