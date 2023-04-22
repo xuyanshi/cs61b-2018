d080d7977325558b72bda7889bb843c65f1bf910
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 19:45:46 +0800

    Update HuffmanDecoder.java

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -1,9 +1,23 @@
+import edu.princeton.cs.algs4.Huffman;
+
 /**
  * @author xuyanshi
  * @date 2023/4/22 17:44
  */
 public class HuffmanDecoder {
     public static void main(String[] args) {
+        // 1: Read the Huffman coding trie.
+
+        // 2: If applicable, read the number of symbols.
+
+        // 3: Read the massive bit sequence corresponding to the original txt.
+
+        // 4: Repeat until there are no more symbols:
+        //    4a: Perform a longest prefix match on the massive sequence.
+        //    4b: Record the symbol in some data structure.
+        //    4c: Create a new bit sequence containing the remaining unmatched bits.
+
+        // 5: Write the symbols in some data structure to the specified file.
         
     }
 }
