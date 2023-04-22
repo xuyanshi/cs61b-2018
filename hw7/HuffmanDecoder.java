a23c56b29d3c5ca0dfe3450ddec9c23cb3435032
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-22 17:45:01 +0800

    add files

---
+++ hw7/HuffmanDecoder.java
@@ -0,0 +1,9 @@
+/**
+ * @author xuyanshi
+ * @date 2023/4/22 17:44
+ */
+public class HuffmanDecoder {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ hw7/HuffmanEncoder.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+/**
+ * @author xuyanshi
+ * @date 2023/4/22 17:43
+ */
+public class HuffmanEncoder {
+    public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
